f66b18cb2df0be4d91d1078485821f87294a55c9
lametje06 <65783439+lametje06@users.noreply.github.com>
2022-03-11 14:58:49 +0100

    Update Meldkamer - Noord Nederland.user.js

--- Meldkamer - Noord Nederland.user.js
+++ Meldkamer - Noord Nederland.user.js
@@ -35,17 +35,17 @@ function addGlobalStyle(css) {
     head.appendChild(style);
 }
 
-//posities van kaart en meldingenlijst
-addGlobalStyle('#map_outer.col-sm-8 { width: 33.33333333% !important; min-height: 860px !important}');
-addGlobalStyle('#map { min-height: 860px !important}');
-addGlobalStyle('#patrolMap { min-height: 860px !important}');
+// Posities van kaart en meldingenlijst
+addGlobalStyle('#map_outer.col-sm-8 { width: 33.33333333% !important; height: 66vh !important}');
+addGlobalStyle('#map { height: 66vh !important}');
+addGlobalStyle('#patrolMap { height: 66vh !important}');
 addGlobalStyle('#missions_outer.col-sm-4 { width: 66.66666667% !important; }');
-addGlobalStyle('#missions-panel-body {height: 816.493px !important}');
+addGlobalStyle('#missions-panel-body {height: 63vh !important}');
 
-//teamfoto
+// Teamfoto
 addGlobalStyle('html > body > div:first-of-type > nav > div:nth-of-type(2) > div:first-of-type > a > img {content: url("http://meldkamer.wesleylambeck.nl/wp-content/uploads/2020/11/favicon.ico") !important ; float: left !important ; transform: scale(1.25); !important ; margin-top: 0px !important }');
 
-// meldkamerspel.com link verwijderen achter logo
+// Meldkamerspel.com link verwijderen achter logo
 addGlobalStyle('html > body > div:first-of-type > nav > div:nth-of-type(2) > div:first-of-type > a:first-of-type {pointer-events: none !important;}');
 
 // MK-NN donkerrood maken als deze aangeklikt is
@@ -55,33 +55,28 @@ addGlobalStyle('.navbar-header a {margin-left: 0px !important}');
 // Menu naar juiste plaats verplaatsen
 addGlobalStyle('html > body > div:first-of-type > nav > div:nth-of-type(2) > div:first-of-type > ul {left: 50px !important ; top: 48px !important ; width: 238px !important}');
 
-// ruimte toevoegen aan logo + teamnaam
+// Ruimte toevoegen aan logo + teamnaam
 addGlobalStyle('html > body > div:first-of-type > nav > div:nth-of-type(2) > div:first-of-type {padding-right: 15px !important}');
 
-// logo en teamnaam uitlijnen zodat deze goed naast elkaar staan
+// Logo en teamnaam uitlijnen zodat deze goed naast elkaar staan
 addGlobalStyle('html > body > div:first-of-type > nav > div:nth-of-type(2) > div:first-of-type {align-items: center !important; justify-content: center !important; display: flex !important}');
 
-// tekstkleur van link op teamnaam weghalen
+// Tekstkleur van link op teamnaam weghalen
 addGlobalStyle('html > body > div:first-of-type > nav > div:nth-of-type(2) > div:first-of-type > a:nth-of-type(2) {text-decoration: none !important; color: white !important;}');
 
 
-// kleuren
-addGlobalStyle('html > body > div:first-of-type .navbar-default {background-color: #D1132F !important }');
-addGlobalStyle('html > body > div:first-of-type > nav > div:nth-of-type(2) > div:first-of-type > ul {background-color: #D1132F !important;}');
-addGlobalStyle('.mission_panel_red {border-left: 5px #D1132F solid !important}');
-addGlobalStyle('.navbar-nav > li > .dropdown-menu {background-color: #D1132F !important}');
-addGlobalStyle('#container_navbar_alarm {background-color: #D1132F !important}');
-addGlobalStyle('.alert-danger {background-image: linear-gradient(to bottom, #f3b2b299 0, #d57e7ea8 100%) !important ; color: #D1132F !important}');
+// Kleuren
+addGlobalStyle('html > body > div:first-of-type .navbar-default {background-color: #D1132F !important }'); // Kleur navigatiebalk bovenaan
+addGlobalStyle('html > body > div:first-of-type > nav > div:nth-of-type(2) > div:first-of-type > ul {background-color: #D1132F !important;}'); // Kleur uitvouwmenu MK-NN
+addGlobalStyle('.mission_panel_red {border-left: 5px #D1132F solid !important}'); // Kleur lint links van meldingen
+addGlobalStyle('.navbar-nav > li > .dropdown-menu {background-color: #D1132F !important}'); // Kleur overige uitvouwmenu's
+addGlobalStyle('#container_navbar_alarm {background-color: #D1132F !important}'); // Kleur onderste balk bij alarmeren van meldingen
+addGlobalStyle('.panel-default > .panel-heading {background-image: linear-gradient(to bottom, #e9e9e9 0, #e7e7e7 100%) }'); // Kleuren achtergrond meldingen wit naar lichtgrijs
+addGlobalStyle('.progress-bar-danger {background-image: linear-gradient(to bottom, #d9534f 0, #D1132F 100%) !important}'); // Kleuren voortgangsbalk hoofdpagina
+addGlobalStyle('html > body.dark.missionchief .alert-danger {background-image: linear-gradient(to bottom, #f3b2b299 0, #d57e7ea8 100%) !important ; color: #FFFFFF !important}'); // Kleuren donkere modus i.c.m. LSS-manager
+addGlobalStyle('html > body.missionchief .alert-danger {background-image: linear-gradient(to bottom, #f3b2b299 0, #d57e7ea8 100%) !important}'); // Kleuren lichte modus i.c.m. LSS-manager
 
-addGlobalStyle('.panel-default > .panel-heading {background-image: linear-gradient(to bottom, #e9e9e9 0, #e7e7e7 100%) }');
-addGlobalStyle('.progress-bar-danger {background-image: linear-gradient(to bottom, #d9534f 0, #D1132F 100%) !important}');
 
-
-// LSS manager kleuren
-//addGlobalStyle('html > body > div:first-of-type > div:nth-of-type(4) > div:nth-of-type(6) > div:first-of-type > div > table > tbody > tr {color: #E74C3C !important}')
-//addGlobalStyle('html > body > div:first-of-type > div:nth-of-type(4) > div:nth-of-type(6) > div:first-of-type > div > table > tbody > tr.overRequirement[data-v-20b2e2aa] {color: #0c0 !important}')
-//addGlobalStyle('html > body > div:first-of-type > div:nth-of-type(4) > div:nth-of-type(6) > div:nth-of-type(2) > div > table > tbody > tr {color: #E74C3C !important}')
-//addGlobalStyle('html > body > div:first-of-type > div:nth-of-type(4) > div:nth-of-type(6) > div:nth-of-type(2) > div > table > tbody > tr.overRequirement[data-v-20b2e2aa] {color: #0c0 !important}')
 
 window.setTimeout(function() {
     map.invalidateSize();
